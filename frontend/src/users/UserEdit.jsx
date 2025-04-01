db2a44fab430c34992fa0ee992b0f60a4be1b45d
Lincoln Araujo <lincoln.araujo@ebc.com.br>
2025-04-01 10:28:36 -0300

    ajustes no editar

--- frontend/src/users/UserEdit.jsx
+++ frontend/src/users/UserEdit.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { Edit, SimpleForm, TextInput, required, email, minLength } from 'react-admin';
+import {
+  Edit,
+  SimpleForm,
+  TextInput,
+  required,
+  email,
+  minLength,
+  useRecordContext,
+} from 'react-admin';
 
 const validateName = required('O nome é obrigatório');
 const validateEmail = [required('O e-mail é obrigatório'), email('E-mail inválido')];
-// Senha será opcional na edição, mas se preenchida, precisa ter no mínimo 6 caracteres
 const validatePassword = minLength(6, 'A senha deve ter no mínimo 6 caracteres');
 
-const UserEdit = (props) => (
-  <Edit {...props}>
-    <SimpleForm>
-      <TextInput source="name" label="Nome" validate={validateName} />
-      <TextInput source="email" label="E-mail" validate={validateEmail} />
-      <TextInput source="password" label="Senha" type="password" validate={validatePassword} />
-    </SimpleForm>
-  </Edit>
-);
+const UserEdit = (props) => {
+  const record = useRecordContext();
+
+  console.log('📌 Dados carregados para edição:', record);
+
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput source="name" label="Nome" validate={validateName} />
+        <TextInput source="email" label="E-mail" validate={validateEmail} />
+        <TextInput source="password" label="Senha" type="password" validate={validatePassword} />
+      </SimpleForm>
+    </Edit>
+  );
+};
 
 export default UserEdit;
